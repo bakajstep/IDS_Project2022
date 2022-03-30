2453d43771f248e7304d6345ffdb3e26e8a35a7b
bakajstep <stepan.bakaj@gmail.com>
2022-03-30 11:51:38 +0200

    fix sql syntax

--- 2/xserej00_xbakaj00.sql
+++ 2/xserej00_xbakaj00.sql
@@ -1,71 +1,64 @@
-TODO
-
-CREATE TABLE `Volny_prodej` (
-  `` <type> 
+CREATE TABLE Zamestnanec (
+  ID INT primary key,
+  Jmeno VARCHAR(50),
+  Prijmeni VARCHAR(50),
+  Mesto VARCHAR(50),
+  PSC VARCHAR(6),
+  Ulice VARCHAR(50),
+  Cislo_popisne INT,
+  Cislo_bankovniho_uctu VARCHAR(25),
+  Predpisovy_prodej NUMBER(1)
 );
 
-CREATE TABLE `Zamestnanec` (
-  `ID` INT primary key,
-  `Jmeno` VARCHAR(50),
-  `Prijmeni` VARCHAR(50),
-  `Mesto` VARCHAR(50),
-  `PSC` VARCHAR(6),
-  `Ulice` VARCHAR(50),
-  `Cislo_popisné` INT,
-  `Cislo_bankovniho_uctu` VARCHAR(25),
-  `Predpisovy_prodej` NUMBER(1)
+CREATE TABLE Prodej (
+  ID INT primary key,
+  Datum TIMESTAMP(2),
+  ID_zamestnance INT,
+  FOREIGN KEY (ID_zamestnance) REFERENCES Zamestnanec(ID)
 );
 
-CREATE TABLE `Prodej` (
-  `ID` INT primary key,
-  `Datum` TIMESTAMP(2),
-  `ID_zamestnance` INT,
-  FOREIGN KEY (`ID_zamestnance`) REFERENCES `Zamestnanec`(`ID`)
+CREATE TABLE Lek (
+  Kod INT primary key,
+  Nazev VARCHAR(50),
+  Vyrobce VARCHAR(50),
+  Lekova_forma VARCHAR(25),
+  Cesta VARCHAR(25),
+  Ucina_latka VARCHAR(50),
+  Velikost_baleni INT,
+  Cena INT,
+  Doplatek INT,
+  Teplota_skladovani INT
 );
 
-CREATE TABLE `Lek` (
-  `Kod` INT primary key,
-  `Nazev` VARCHAR(50),
-  `Vyrobce` VARCHAR(50),
-  `Lekova_forma` VARCHAR(25),
-  `Cesta` VARCHAR(25),
-  `Ucina_latka` VARCHAR(50),
-  `Velikost_baleni` INT,
-  `Cena` INT,
-  `Teplota_skladovani` INT
+CREATE TABLE Zasoby (
+  ID INT primary key,
+  Datum_spotreby DATE not null,
+  Mnozstvi INT,
+  Kod_leku INT not null,
+  FOREIGN KEY (Kod_leku) REFERENCES Lek(Kod)
 );
 
-TODO
-
-CREATE TABLE `Na_predpis` (
-  `Doplatek` INT
+CREATE TABLE Telefon (
+  ID_zamestnance INT,
+  Poradove_cislo INT,
+  Telefon VARCHAR(25),
+  Popis VARCHAR(255),
+  primary key(ID_zamestnance, Poradove_cislo)
 );
 
-CREATE TABLE `Zasoby` (
-  `ID` INT primary key,
-  `Datum spotřeby` DATE not null,
-  `Množství` INT,
-  `Kod_leku` INT not null,
-  FOREIGN KEY (`Kod_leku`) REFERENCES `Lek`(`Kod`)
+CREATE TABLE Pojistovna (
+  Kod INT primary key,
+  Nazev VARCHAR(255)
 );
 
-CREATE TABLE `Telefon` (
-  `Poradove_cislo` int,
-  `Telefon` VARCHAR(25),
-  `Popis` VARCHAR(255)
-);
-
-CREATE TABLE `Pojistovna` (
-  `Kod` INT primary key,
-  `Nazev` VARCHAR(255)
-);
-
-CREATE TABLE `zasoby-prodej` (
-  `Kod_pojistovny` INT not null,
-  `ID_prodeje` INT,
-  `Cislo_pojistence` VARCHAR(10),
-  primary key(ID_prodeje, Kod_pojistovny),
-  FOREIGN KEY (`Kod_pojistovny`) REFERENCES `Pojistovna`(`Kod`),
-  FOREIGN KEY (`ID_prodeje`) REFERENCES `Prodej`(`ID`)
+CREATE TABLE zasoby_prodej (
+  ID_prodeje INT,
+  ID_zasoby INT,
+  Kod_pojistovny INT,
+  Cislo_pojistence VARCHAR(10),
+  primary key(ID_prodeje, ID_zasoby),
+  FOREIGN KEY (Kod_pojistovny) REFERENCES Pojistovna(Kod),
+  FOREIGN KEY (ID_prodeje) REFERENCES Prodej(ID),
+  FOREIGN KEY (ID_zasoby) REFERENCES Zasoby(ID)
 );
 
